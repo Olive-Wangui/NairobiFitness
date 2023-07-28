f4deed5144532025c5b5ca8e782b0f14cd0c081a
Olive-Wangui <owangui4oly@gmail.com>
2023-07-28 22:04:57 +0300

    All files

--- nairobifitness/authapp/urls.py
+++ nairobifitness/authapp/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from authapp import views
 
 urlpatterns = [
-   path('', views.Home, name="Home")
+   path('', views.Home, name="Home"),
+   path('signup', views.signup, name="signup"),
+   path('login', views.handle_login, name="handle_login"),
+   path('logout', views.handle_logout, name="handle_logout"),
+   path('contact', views.contact, name="contact"),
+   path('join', views.enroll, name="enroll"),
+   path('profile', views.profile, name="profile"),
+   path('attendance',views.attendance,name="attendance"),
+    path('gallery',views.gallery,name="gallery")
 ]
